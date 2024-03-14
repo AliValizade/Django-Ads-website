8f89a44952d5dbd85a2658dd59398739bf68baf2
AliValizade <alivalizade@gmail.com>
2024-03-15 02:13:45 +0330

    add signup and logout

--- config/settings.py
+++ config/settings.py
@@ -136,4 +136,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.CustomUser'
 
 LOGIN_REDIRECT_URL = 'login'
-# LOGOUT_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
