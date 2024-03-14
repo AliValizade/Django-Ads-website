197e0955b9dfcde2d7f7d05178381c16b56fc430
AliValizade <alivalizade@gmail.com>
2024-03-15 00:43:33 +0330

    add login

--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,3 +134,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.CustomUser'
+
+LOGIN_REDIRECT_URL = 'login'
+# LOGOUT_REDIRECT_URL = 'home'
